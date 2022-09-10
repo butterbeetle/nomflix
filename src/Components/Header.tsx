3edd226dbbc37b93a39e273cf993982579b343f9
butterbeetle <butterbeetle@naver.com>
2022-09-11 02:20:02 +0900

    Search Redirect

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 import { motion, useAnimation, useScroll } from "framer-motion";
-import { Link, useMatch } from "react-router-dom";
+import { Link, useMatch, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Nav = styled(motion.nav)`
   display: flex;
@@ -50,7 +51,7 @@ const Item = styled.li`
   }
 `;
 
-const Search = styled.span`
+const Search = styled.form`
   color: white;
   display:flex;
   align-items: center;
@@ -106,6 +107,10 @@ const navVariants = {
   },
 }
 
+interface IForm {
+  keyword: string;
+}
+
 function Header() {
   const [serach, setSearch] = useState(false);
   const homeMatch = useMatch("/");
@@ -136,6 +141,11 @@ function Header() {
       }
     });
   }, [scrollY, navAnimation])
+  const navigate = useNavigate();
+  const { register, handleSubmit } = useForm<IForm>();
+  const onValid = (data: IForm) => {
+    navigate(`/search?keyword=${data.keyword}`)
+  }
   return (
     <Nav
       variants={navVariants}
@@ -170,7 +180,7 @@ function Header() {
         </Items>
       </Col>
       <Col>
-        <Search>
+        <Search onSubmit={handleSubmit(onValid)}>
           <motion.svg
             onClick={toggleSerach}
             animate={{ x: serach ? -210 : 0 }}
@@ -186,6 +196,7 @@ function Header() {
             ></path>
           </motion.svg>
           <Input
+            {...register("keyword", { required: true, minLength: 2 })}
             initial={{ scaleX: 0 }}
             animate={inputAnimation}
             transition={{ type: "linear" }}
