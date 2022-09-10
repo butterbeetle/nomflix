067d751f8ddd14946229427c7a407b3c4d77514e
butterbeetle <butterbeetle@naver.com>
2022-09-11 01:09:56 +0900

    Box Animations part Two

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -56,8 +56,8 @@ const Box = styled(motion.div) <{ bgPhoto: string }>`
   background-image: url(${props => props.bgPhoto});
   background-size: cover;
   background-position: center center;
-  height: 150px;
-  width: 250px;
+  height: 200px;
+  font-size: 66px;
   &:first-child{
     transform-origin: center left;
   }
@@ -66,32 +66,55 @@ const Box = styled(motion.div) <{ bgPhoto: string }>`
   }
 `;
 
+const Info = styled(motion.div)`
+  padding: 10px;
+  background-color: ${(props) => props.theme.black.lighter};
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  h4 {
+    text-align: center;
+    font-size: 18px;
+  }
+`;
+
 const rowVariants = {
   hidden: {
-    x: window.outerWidth,
+    x: window.outerWidth + 5,
   },
   visible: {
     x: 0,
   },
   exit: {
-    x: -window.outerWidth,
+    x: -window.outerWidth - 5,
   },
-}
-
+};
 const boxVariants = {
   normal: {
     scale: 1,
   },
   hover: {
-    scale: 1.4,
-    y: -50,
+    scale: 1.3,
+    y: -80,
     transition: {
       delay: 0.5,
-      duration: 0.3,
+      duaration: 0.1,
       type: "tween",
-    }
-  }
-}
+    },
+  },
+};
+
+const infoVariants = {
+  hover: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      duaration: 0.1,
+      type: "tween",
+    },
+  },
+};
 
 const offset = 6;
 
@@ -142,11 +165,15 @@ function Home() {
                     whileHover="hover"
                     initial="normal"
                     variants={boxVariants}
-                    transition={{
-                      type: "tween",
-                    }}
+                    transition={{ type: "tween" }}
                     bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
-                  />
+                  >
+                    <Info
+                      variants={infoVariants}
+                    >
+                      <h4>{movie.title}</h4>
+                    </Info>
+                  </Box>
                 )}
               </Row>
             </AnimatePresence>
